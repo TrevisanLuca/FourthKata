8d89a3ff20490b8314c1d87449ff10c68e1cba78
Luca Trevisan <trevisan@euris.it>
2022-02-10 13:18:17 +0100

    Compressed CitySearch method

--- TDDFourthKata/FourthKata/CitySearcher.cs
+++ TDDFourthKata/FourthKata/CitySearcher.cs
@@ -8,19 +8,25 @@ namespace FourthKata
 {
     public class CitySearcher : ICitySearcher
     {
+        //public List<string> CitySearch(string input)
+        //{
+        //    if (input == "*") return CitiesList.CityList;
+        //    if (input.Length <= 1) return new List<string>();
+        //    var result = new List<string>();
+        //    input = input.ToLower();
+        //    foreach (string item in CitiesList.CityList)
+        //    {
+        //        if (item.ToLower().Contains(input))
+        //            result.Add(item);
+        //    }
+
+        //    return result;
+        //}
         public List<string> CitySearch(string input)
         {
             if (input == "*") return CitiesList.CityList;
             if (input.Length <= 1) return new List<string>();
-            var result = new List<string>();
-            input = input.ToLower();
-            foreach (string item in CitiesList.CityList)
-            {
-                if (item.ToLower().Contains(input))
-                    result.Add(item);
-            }
-
-            return result;
+            return CitiesList.CityList.Where(x => x.ToLower().Contains(input.ToLower())).ToList();
         }
     }
 }
